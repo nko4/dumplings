93a28d777c7e0e717e48957020033bdb05d209df
piecioshka <piecioshka@gmail.com>
2013-11-09 15:56:50 +0100

    add feature

--- public/scripts/items/Bomb.js
+++ public/scripts/items/Bomb.js
@@ -18,6 +18,7 @@ define([], function () {
         create: function () {
             // this.tile = this.game.add.sprite(this.x, this.y, 'bomb');
             this.tile = this.bombs.create(this.x, this.y, 'bomb');
+            // this.tile.body.immovable = true; // disable, more fun
             setTimeout(function () {
                 this.destroy();
             }.bind(this), 3000);
