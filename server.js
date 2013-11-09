60ae44266df1c75ccbaffc0e6cac8caa454d362a
kamil <kamil.zaleski@gmail.com>
2013-11-09 20:11:47 +0100

    x

--- server.js
+++ server.js
@@ -42,23 +42,7 @@ app.configure(function(){
 });
 
 
-if (isProduction) {
-  io.enable('browser client minification');  // send minified client
-  io.enable('browser client etag');          // apply etag caching logic based on version number
-  io.enable('browser client gzip');          // gzip the file
-  io.set('log level', 1);                    // reduce logging
 
-  // enable all transports (optional if you want flashsocket support, please note that some hosting
-  // providers do not allow you to create servers that listen on a port different than 80 or their
-  // default port)
-  io.set('transports', [
-      'websocket'
-    , 'flashsocket'
-    , 'htmlfile'
-    , 'xhr-polling'
-    , 'jsonp-polling'
-  ]);
-}
 
 
 var Game;
@@ -157,6 +141,24 @@ move_block = false;
 
 var server = app.listen(port);
 var io = require('socket.io').listen(server);
+
+if (isProduction) {
+  io.enable('browser client minification');  // send minified client
+  io.enable('browser client etag');          // apply etag caching logic based on version number
+  io.enable('browser client gzip');          // gzip the file
+  io.set('log level', 1);                    // reduce logging
+
+  // enable all transports (optional if you want flashsocket support, please note that some hosting
+  // providers do not allow you to create servers that listen on a port different than 80 or their
+  // default port)
+  io.set('transports', [
+      'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+  ]);
+}
 
 app.get('/', function (reseq, res) {
     res.render('index', { isProduction: isProduction })
