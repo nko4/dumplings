2a139116ef36624e462c34e6fe2cb4947c16e0aa
piecioshka <piecioshka@gmail.com>
2013-11-09 07:07:05 +0100

    pikatchu is moving

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,16 @@
 define([
-    'phaser'
-], function (Phaser) {
+    'phaser',
+    'items/Actor'
+], function (Phaser, Actor) {
     'use strict';
 
     var App = function (callback) {
         this.callback = callback;
+        this.cursors = null;
         this.initialize();
     };
+
+    var player;
 
     App.prototype = {
         initialize: function () {
@@ -14,7 +18,9 @@ define([
 
             this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'phaser-example', {
                 preload: this.preload.bind(this),
-                create: this.create.bind(this)
+                create: this.create.bind(this),
+                update: this.update.bind(this),
+                render: this.render.bind(this)
             });
         },
         preload: function () {
@@ -24,12 +30,39 @@ define([
         create: function () {
             log('create process');
 
-            this.game.add.sprite(50, 50, 'pikatchu');
+            this.game.world.setBounds(0, 0, 2000, 1000); // world size
+            this.game.stage.backgroundColor = "#0c0c0c"; // world color
+
+            this.cursors = this.game.input.keyboard.createCursorKeys();
 
             this.callback.call(this);
         },
-        addActor: function (name) {
+        update: function () {
+            player.tile.body.velocity.setTo(0, 0);
+
+            if (this.cursors.up.isDown) {
+                player.tile.body.velocity.y = -200;
+            } else if (this.cursors.down.isDown) {
+                player.tile.body.velocity.y = 200;
+            }
+
+            if (this.cursors.left.isDown) {
+                player.tile.body.velocity.x = -200;
+            } else if (this.cursors.right.isDown) {
+                player.tile.body.velocity.x = 200;
+            }
+        },
+        // run per each mouse move on game board
+        render: function () {
+            this.game.debug.renderCameraInfo(this.game.camera, 32, 32);
+            this.game.debug.renderSpriteCoords(player.tile, 32, 200);
+        },
+        addActor: function (name, sprite) {
             console.log('add actor %s', name);
+            sprite = sprite || 'pikatchu';
+            player = new Actor(this.game, name, sprite);
+            player.tile.body.collideWorldBounds = true; // disable go out of world
+            this.game.camera.follow(player.tile);
         }
     };
 
--- public/scripts/items/Actor.js
+++ public/scripts/items/Actor.js
@@ -1,12 +1,18 @@
 define([], function () {
     'use strict';
 
-    var Actor = function () {
+    var Actor = function (game, name, sprite) {
+        this.tile = null;
+        this.game = game;
+        this.name = name;
+        this.sprite = sprite;
 
+        this.create();
     };
     Actor.prototype = {
         create: function () {
             log('actor create');
+            this.tile = this.game.add.sprite(50, 50, 'pikatchu');
         }
     };
     return Actor;
