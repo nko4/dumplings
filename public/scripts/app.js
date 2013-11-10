27c57438c10b9c005c8b5274782af42dc78f5048
piecioshka <piecioshka@gmail.com>
2013-11-10 23:47:10 +0100

    new assets for players

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,9 +51,9 @@ define([
             this.game.load.spritesheet('brick', 'assets/pics/2/brick.png', 50, 50, 4);
             this.game.load.spritesheet('mixture', 'assets/pics/2/mixture.png', 50, 50, 3);
 
-            this.game.load.spritesheet('player', 'assets/pics/1/fighter-1.png', 35, 35, 4);
-            this.game.load.spritesheet('opponent', 'assets/pics/1/fighter-2.png', 35, 35, 4);
-            this.game.load.spritesheet('special', 'assets/pics/1/fighter-3.png', 35, 35, 4);
+            this.game.load.spritesheet('player', 'assets/pics/2/fighter-1.png', 32, 32, 4);
+            this.game.load.spritesheet('opponent', 'assets/pics/2/fighter-2.png', 32, 32, 4);
+            this.game.load.spritesheet('special', 'assets/pics/2/fighter-3.png', 32, 32, 4);
 
             this.game.load.audio('music', ['assets/audio/sound.wav', 'assets/audio/sound.mp3', 'assets/audio/sound.ogg']);
         },
