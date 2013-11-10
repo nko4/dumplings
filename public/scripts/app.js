d5f3789b20d02c318302449e366a1805083dd804
piecioshka <piecioshka@gmail.com>
2013-11-10 18:51:52 +0100

    control plant a bomb

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -71,8 +71,8 @@ define([
             this.game.stage.backgroundColor = '#0c0c0c'; // color of world
 
             this.cursors = this.game.input.keyboard.createCursorKeys(); // handle moving
-            var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-            spaceKey.onDown.add(function () { player.plantBomb(); }, this);
+            var ctrlKey = this.game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
+            ctrlKey.onDown.add(function () { player.plantBomb(); }, this);
 
             this._callback.call(this);
         },
