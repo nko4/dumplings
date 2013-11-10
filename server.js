44cfde749d706178e813b75af61baaef38a88424
piecioshka <piecioshka@gmail.com>
2013-11-10 09:29:53 +0100

    kamil - use strict tylko pomaga, a nie przeszkadza - to NIE JEST jak JSLint ktory Ci mowi, ze za duzo spacji napisales, tylko sprawdza m.in poprawnosc typow i scopy zmienny czy sa wlasciwe - musisz uzywac strict mode - pomagasz tym tylko sobie

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('WfsRuxlWWBoW0L63');
 
+'use strict';
+
 var isProduction = (process.env.NODE_ENV === 'production');
 var http = require('http');
 var port = (isProduction ? 80 : 8000);
@@ -27,7 +29,6 @@ var app = express();
 var config = {};
 
 
-
 app.configure(function(){
   //app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -41,8 +42,7 @@ app.configure(function(){
   // app.use(require('connect-assets')());
 });
 
-var globalDb = null;
-globalUri = 'mongodb://nko:nko@paulo.mongohq.com:10006/nko';
+var globalUri = 'mongodb://nko:nko@paulo.mongohq.com:10006/nko';
 
 MongoClient.connect(globalUri, function(err, db) {
   if(err) throw err;
@@ -53,20 +53,19 @@ MongoClient.connect(globalUri, function(err, db) {
 });
 
 
-var Game;
-
-Game = (function() {
+var Game = (function() {
   function Game (argument) {
+    var self = this;
 
-    map = [];
+    var map = [];
 
     // 0 - space
     // 1 - wall
     // 2 - brick
     // 3 - power
 
-    MAP_X = 44;
-    MAP_Y = 30;
+    var MAP_X = 44;
+    var MAP_Y = 30;
 
     // MAP_X = 24;
     // MAP_Y = 13;
@@ -77,20 +76,20 @@ Game = (function() {
     this.brickCount = 0;
     this.wallCount = 0;
     this.powerCount = 0;
-    this.maxCount = MAP_X*MAP_Y;
+    this.maxCount = MAP_X * MAP_Y;
 
 
     for (var x = MAP_X; x >= 0; x--) {
       map[x] = [];
       for (var y = MAP_Y; y >= 0; y--) {
         map[x].push(0);
-      };
-    };
+      }
+    }
 
-    _.times(MAP_X, function (x) { map[x][0] = 1;      this.wallCount += 1; });
-    _.times(MAP_X, function (x) { map[x][MAP_Y] = 1;  this.wallCount += 1; });
-    _.times(MAP_Y, function (y) { map[0][y] = 1;      this.wallCount += 1; });
-    _.times(MAP_Y, function (y) { map[MAP_X][y] = 1;  this.wallCount += 1; });
+    _.times(MAP_X, function (x) { map[x][0] = 1;      self.wallCount += 1; });
+    _.times(MAP_X, function (x) { map[x][MAP_Y] = 1;  self.wallCount += 1; });
+    _.times(MAP_Y, function (y) { map[0][y] = 1;      self.wallCount += 1; });
+    _.times(MAP_Y, function (y) { map[MAP_X][y] = 1;  self.wallCount += 1; });
     _.times(MAP_X, function (n) {
       if (!n) return;
       if (n % 2) return;
@@ -102,60 +101,56 @@ Game = (function() {
         if (m === MAP_Y - 1) return;
 
         map[n][m] = 1;
-        this.wallCount += 1;
-                    
+        self.wallCount += 1;
       });
     });
 
-    map[MAP_X][MAP_Y] = 1; //hack :)
+    map[MAP_X][MAP_Y] = 1; // hack :) - brawo Kamil!
+
     this.wallCount += 1;
     this.maxCount -= this.wallCount;
     
     _.times(parseInt(MAP_X*MAP_Y*0.50),function() {
       var x = Math.floor(Math.random() * MAP_X-1) + 1;
       var y = Math.floor(Math.random() * MAP_Y-1) + 1;
-      var elem = map[x][y];
+      // var elem = map[x][y];
 
       if (map[x][y] == 0) {
         map[x][y] = 2;
-        this.brickCount += 1;
+        self.brickCount += 1;
       }
     });
 
     _.times(parseInt(MAP_X*MAP_Y*0.05),function() {
       var x = Math.floor(Math.random() * MAP_X-1) + 1;
       var y = Math.floor(Math.random() * MAP_Y-1) + 1;
-      var elem = map[x][y];
+      // var elem = map[x][y];
 
       if (map[x][y] == 0) {
         map[x][y] = 3;
-        this.powerCount += 1;
+        self.powerCount += 1;
       }
     });
 
     this.map = map;
     this.players = {};
-    this.nextId = 0;
-
+    // this.nextId = 0;
   }
 
   Game.prototype.randNewBrick = function() {
     if ( this.brickCount < (this.maxCount/2) ) {
-      
       var x = Math.floor(Math.random() * this.MAP_X-1) + 1;
       var y = Math.floor(Math.random() * this.MAP_Y-1) + 1;
-      var elem = map[x][y];
+      // var elem = map[x][y];
 
-      if (map[x][y] == 0) {
-        map[x][y] = 2;
+      if (this.map[x][y] == 0) {
+        this.map[x][y] = 2;
         this.brickCount += 1;
         return [x,y];
       }
-
     }
-
     return false;
-  }
+  };
 
 
   Game.prototype.randNewPower = function() {
@@ -163,22 +158,19 @@ Game = (function() {
       
       var x = Math.floor(Math.random() * this.MAP_X-1) + 1;
       var y = Math.floor(Math.random() * this.MAP_Y-1) + 1;
-      var elem = map[x][y];
+      // var elem = map[x][y];
 
-      if (map[x][y] == 0) {
-        map[x][y] = 3;
+      if (this.map[x][y] == 0) {
+        this.map[x][y] = 3;
         this.powerCount += 1;
         return [x,y];
       }
-
     }
-
     return false;
-  }
+  };
 
 
   Game.prototype.set = function(x,y,value) {
-    
     if (this.map[x][y] == 2) {
       this.brickCount -= 1;
     }
@@ -188,20 +180,14 @@ Game = (function() {
     }
 
     this.map[x][y] = value;
-
   };
 
   return Game;
-  // Game.prototype.
-
 })();
 
 var game = new Game(1);
 
-
-var Player, player;
-
-Player = (function() {
+var Player = (function() {
   function Player(x, y) {
     this.x = x;
     this.y = y;
@@ -215,10 +201,9 @@ Player = (function() {
   };
 
   return Player;
-
 })();
 
-move_block = false;
+var move_block = false;
 
 var server = app.listen(port);
 var io = require('socket.io').listen(server);
@@ -264,7 +249,7 @@ io.sockets.on('connection', function (socket) {
   socket.on('kill',function(id) {
     delete game.players[id];
     socket.broadcast.emit('killed',{ id: id, by_id: socket.id })
-  })
+  });
 
   socket.on('pm',function(x,y) {
     
@@ -295,17 +280,19 @@ io.sockets.on('connection', function (socket) {
 });
 
 setInterval(function() {
+  var new_brick;
 
-  var new_brick = game.randNewBrick();
+  // build ne brick
+  new_brick = game.randNewBrick();
 
 
   if (new_brick) {
     io.sockets.emit('mc',new_brick[0],new_brick[1],2); 
   }
 
-  var new_brick = game.randNewPower();
+  new_brick = game.randNewPower();
 
-
+  // build ne brick
   if (new_brick) {
     io.sockets.emit('mc',new_brick[0],new_brick[1],3); 
   }
